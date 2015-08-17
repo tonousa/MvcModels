b0117d5273bce60a2cbfed20fce6e2e3ab74260b
tonousa <tonousa@hotmail.com>
2015-08-17 09:13:43 -0400

    585

---
+++ MvcModels/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using MvcModels.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcModels.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Person[] = {
+            new Person { }
+        };
+
+        public ActionResult Index(int id)
+        {
+            Person dataItem = personData
+            return View();
+        }
+
+    }
+}
